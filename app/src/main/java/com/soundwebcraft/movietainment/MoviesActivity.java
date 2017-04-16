3261dfaeee37537cf41e7096bc97b2ee589de0fb
Olushi Luqman O <olushilukmon03@gmail.com>
2017-04-16 21:35:07 +0100

    Initial commit

---
+++ app/src/main/java/com/soundwebcraft/movietainment/MoviesActivity.java
@@ -0,0 +1,13 @@
+package com.soundwebcraft.movietainment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
