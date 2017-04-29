3611b3b08d5d096ac2b883b214e6aace730a6c78
Olushi Luqman O <olushilukmon03@gmail.com>
2017-04-29 16:39:12 +0100

    wired up movie trailers adapter

--- app/src/main/java/com/soundwebcraft/movietainment/adapters/MovieTrailersAdapter.java
+++ app/src/main/java/com/soundwebcraft/movietainment/adapters/MovieTrailersAdapter.java
@@ -1,31 +1,64 @@
 package com.soundwebcraft.movietainment.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.soundwebcraft.movietainment.R;
+import com.soundwebcraft.movietainment.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MovieTrailersAdapter extends RecyclerView.Adapter<MovieTrailersAdapter.TrailerViewHolder> {
+    private Context mContext;
+    private List<Movie> mTrailers;
+
+    public MovieTrailersAdapter(Context context, List<Movie> trailers) {
+        this.mContext = context;
+        mTrailers  = trailers;
+    }
 
     @Override
     public MovieTrailersAdapter.TrailerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_movie_trailer, parent, false);
+
+        return (new TrailerViewHolder(view));
     }
 
     @Override
     public void onBindViewHolder(MovieTrailersAdapter.TrailerViewHolder holder, int position) {
-
+        Movie movie = mTrailers.get(position);
+        ImageView trailerIv = holder.trailerPoster;
+        Picasso.with(mContext)
+                .load(movie.getTrailerThumbnail())
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.no_preview)
+                .into(trailerIv);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mTrailers.size();
     }
 
     public class TrailerViewHolder extends RecyclerView.ViewHolder {
+        // get reference to views
+        @BindView(R.id.movie_trailer_poster)
+        ImageView trailerPoster;
 
         public TrailerViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
