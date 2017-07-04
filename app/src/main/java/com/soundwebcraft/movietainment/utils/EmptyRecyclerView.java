415c6a662a4e1ca58e96d121c6800769dc155c40
Olushi Luqman O <olushilukmon03@gmail.com>
2017-07-04 10:34:07 +0100

    autofit recyclerview grid layout manager

--- app/src/main/java/com/soundwebcraft/movietainment/utils/EmptyRecyclerView.java
+++ app/src/main/java/com/soundwebcraft/movietainment/utils/EmptyRecyclerView.java
@@ -1,12 +1,16 @@
 package com.soundwebcraft.movietainment.utils;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class EmptyRecyclerView extends RecyclerView {
 
+    private GridLayoutManager manager;
+    private int columnWidth = -1;
     private View emptyView;
 
     final private AdapterDataObserver observer = new AdapterDataObserver() {
@@ -28,15 +32,18 @@ public class EmptyRecyclerView extends RecyclerView {
 
     public EmptyRecyclerView(Context context) {
         super(context);
+        init(context, null);
     }
 
     public EmptyRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs);
     }
 
     public EmptyRecyclerView(Context context, AttributeSet attrs,
                              int defStyle) {
         super(context, attrs, defStyle);
+        init(context, attrs);
     }
 
     void checkIfEmpty() {
@@ -65,5 +72,29 @@ public class EmptyRecyclerView extends RecyclerView {
     public void setEmptyView(View emptyView) {
         this.emptyView = emptyView;
         checkIfEmpty();
+    }
+
+
+    private void init(Context context, AttributeSet attrs) {
+        if (attrs != null) {
+            int[] attrsArray = {
+                    android.R.attr.columnWidth
+            };
+            TypedArray array = context.obtainStyledAttributes(attrs, attrsArray);
+            columnWidth = array.getDimensionPixelSize(0, -1);
+            array.recycle();
+        }
+
+        manager = new GridLayoutManager(getContext(), 1);
+        setLayoutManager(manager);
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+        super.onMeasure(widthSpec, heightSpec);
+        if (columnWidth > 0) {
+            int spanCount = Math.max(1, getMeasuredWidth() / columnWidth);
+            manager.setSpanCount(spanCount);
+        }
     }
 }
